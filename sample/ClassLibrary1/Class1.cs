50c7302d4f3b14c84d7f4c439b0652139bea4a9b
udhaya2307 <udhayakumar.p@metamation.com>
2023-08-24 17:14:39 +0530

    Cleanup

---
+++ sample/ClassLibrary1/Class1.cs
@@ -0,0 +1,35 @@
+﻿namespace DebuggingSample;
+
+class StringOp
+{
+    static void Main(string[] args)
+    {
+
+        string[] texts = { "WELCOME", "to introduction", "to Visual studio", "& Debugging" };
+        foreach (string txt in texts)
+        {
+            Console.WriteLine($"Input string : {txt}");
+            var reverse = Reverse(txt);
+            Console.WriteLine($"Reversed string : {reverse}");
+            var vowelCnt = GetVowelCnt(txt);
+            Console.WriteLine($"Number of vowels in string : {vowelCnt}");
+            var isPalindrome = IsPalindrome(txt);
+            Console.WriteLine($"Is palindrome: {isPalindrome}");
+            Console.WriteLine();
+            int a = 1, b = 2, c = 30;
+            for (int i = 0; i < 10; i++) { a++; b *= 2; c--; }
+        }
+    }
+
+    static string Reverse(string s) => new string(s.Reverse().ToArray());
+
+    static int GetVowelCnt(string s)
+    {
+        int cnt = 0;
+        char[] vowels = { 'a', 'e', 'i', 'o', 'u' };
+        foreach (char ch in s.ToLower()) if (vowels.Contains(ch)) cnt++;
+        return cnt;
+    }
+
+    static bool IsPalindrome(string s) => s == Reverse(s);
+}
